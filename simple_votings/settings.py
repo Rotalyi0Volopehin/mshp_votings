9871ec92a2b3ade8d493eb6e9897cfd4f298eb13
Alexander Panov <ironsto44@gmail.com>
2020-01-19 14:09:49 +0300

    Reguirements addition

--- simple_votings/settings.py
+++ simple_votings/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    'bootstrap4',
-    'django_extensions'
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
